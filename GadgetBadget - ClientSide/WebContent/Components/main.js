8f7dc61e06301e04ab047b9e750c1bfbc299dec8
PasinduManeka <manekaherat815@gmail.com>
2021-05-14 18:45:49 +0530

    set the validation

--- GadgetBadget - ClientSide/WebContent/Components/main.js
+++ GadgetBadget - ClientSide/WebContent/Components/main.js
@@ -6,3 +6,27 @@ $(document).ready(function(){
 });
 
 //Validation 
+function validateFundForm(){
+	//code
+	if($("#cartID").val().trim() == ""){
+		return "Insert the cart id";
+	}
+	
+	//researcher name
+	if($("#rName").val().trim() == ""){
+		return "Insert the Researcher Name";
+	}
+	
+	//Invest amount
+	if($("#iAmount").val().trim() == ""){
+		return "Insert the invest amount";
+	}
+	
+	//description
+	if($("#description").val().trim() == ""){
+		return "Insert the description";
+	}
+	
+	
+	
+}
