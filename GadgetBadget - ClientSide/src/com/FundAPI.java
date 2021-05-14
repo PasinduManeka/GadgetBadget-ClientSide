3313d48cc9a0af6d56f8732898cf660fe35cc9b5
PasinduManeka <manekaherat815@gmail.com>
2021-05-14 18:15:35 +0530

    set the map static.

--- GadgetBadget - ClientSide/src/com/FundAPI.java
+++ GadgetBadget - ClientSide/src/com/FundAPI.java
@@ -1,11 +1,17 @@
 package com;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Fund;
 
 /**
  * Servlet implementation class FundAPI
@@ -36,6 +42,9 @@ public class FundAPI extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		Fund fund = new Fund();
+		String output = fund.insertFunds(request.getParameter("cartID"), request.getParameter("rName"), request.getParameter("iAmount"), request.getParameter("description"));
+		response.getWriter().write(output);
 	}
 
 	/**
@@ -50,6 +59,24 @@ public class FundAPI extends HttpServlet {
 	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	}
+	
+	private static Map getParaMap(HttpServletRequest request) {
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+			scanner.close();
+			
+			String[] params = queryString.split("&");
+			for(String param : params) {
+				String[] p = param.split("=");
+				map.put(p[0], p[1]);
+			}
+		}catch(Exception e) {
+			
+		}
+		return map;
 	}
 
 }
